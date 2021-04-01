4e03432ad97486aa9f4e6478bdf3ec3c2d5f1f67
Jana Gjelevska <jana.gelevska@hotmail.com>
2021-04-02 00:42:40 +0200

    example codetinkering before idp change

---
+++ src/main/java/saml/demo/spmockup/SpMockupApplication.java
@@ -0,0 +1,13 @@
+package saml.demo.spmockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpMockupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpMockupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/saml/demo/spmockup/SpMockupApplicationTests.java
@@ -0,0 +1,13 @@
+package saml.demo.spmockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpMockupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
